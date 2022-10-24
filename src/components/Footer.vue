36189a5f37517a38b09eda5f1ee8ed9169aa17d8
AlekeyJr <agarciap24@miumg.edu.gt>
2022-10-23 20:49:55 -0600

    uwuntu

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+
+        <div>
+            <footer>
+
+               Tarea
+               Alex Rene Garcia Porras
+               1290-19-2880
+
+             </footer>
+
+        </div>
+
+</template>
+<style  scoped>
+footer {
+    position: fixed;
+    height: 75px;
+    bottom: 0;
+    width: 90%;
+    background-color: green;
+    color: blanchedalmond;
+}
+</style>
